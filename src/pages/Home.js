5f676dbd72127ee47bbbe7d98b69fd71c34d8265
renercrows <ungredda95@gmail.com>
2022-08-17 22:32:35 -0400

    Fixx CSS Errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,9 @@ import '../index.css'
 
 
 const Home = () => (
-  <div className='home'>
+  <div className="home">
     <h2>Welcome to our page!</h2>
-    <p className='text'>
+    <p className="text">
       Hello there, math magician! This is the Math Magicians web app. The web app is simple. It
       has three pages, this is the
       <strong> Home </strong>
@@ -19,7 +19,7 @@ const Home = () => (
       <strong> Quote </strong>
       page. Where you will find a quote from a famous math magician.
     </p>
-    <p className='text2'>
+    <p className="text2">
       I hope you enjoy this website. Lets explore the other pages, click on the links above!
     </p>
   </div>
