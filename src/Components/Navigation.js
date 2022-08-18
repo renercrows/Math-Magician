430879a12e42a5baa85c00a14cb8cc2c1d1e56cd
renercrows <ungredda95@gmail.com>
2022-08-17 22:15:02 -0400

    Add Eslint Exception

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../index.css'
+import '../index.css';
 
 function Navigation() {
   return (
--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -2,11 +2,11 @@ import React from 'react';
 import '../index.css';
 
 const Quote = () => (
-  <section id='quote'>
+  <section id="quote">
     <p>
       &quot;Pure mathematics is, in its way, the poetry of logical ideas.&quot;
     </p>
-    <span className='author'>Albert Einstein</span>
+    <span className="author">Albert Einstein</span>
   </section>
 );
 export default Quote;
